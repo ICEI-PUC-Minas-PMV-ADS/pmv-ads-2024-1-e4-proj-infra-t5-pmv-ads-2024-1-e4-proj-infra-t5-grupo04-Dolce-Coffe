59ed81dda6aa8cb78293e440d8c755f8b0585856
vinicio machado muniz de morais <viniciomorais18@gmail.com>
2024-04-27 15:20:24 -0300

    Reapply "Criação do Carrossel e algumas Mudanças"
    This reverts commit 18de09090d468416c1d5526c2d811beec031c501.

--- src/Cart/cart.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const cartButton = document.getElementById('cartButton');
-    cartButton.addEventListener('click', function() {
-        window.open('/src/Cart/index.html', '_blank');
-    });
-});
