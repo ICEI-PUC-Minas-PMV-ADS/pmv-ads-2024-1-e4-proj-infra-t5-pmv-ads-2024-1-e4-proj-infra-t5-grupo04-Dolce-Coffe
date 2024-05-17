35c222ab523ac6ed6c8a324ed3e1a6e8edaba22b
VictorKingma <victormkingma@gmail.com>
2024-05-16 23:24:55 -0300

    'requisicoes'

--- frontmobile/app/(tabs)/explore.tsx
+++ frontmobile/app/(tabs)/explore.tsx
@@ -8,6 +8,8 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function TabTwoScreen() {
+
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
--- frontmobile/app/(tabs)/index.tsx
+++ frontmobile/app/(tabs)/index.tsx
@@ -20,7 +20,7 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">Step 1: Try it teste</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
